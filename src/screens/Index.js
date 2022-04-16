c062ad1fe06da57f2374172e8ce794568f684036
fazalpluton <95477050+fazalpluton@users.noreply.github.com>
2022-04-16 05:38:23 +0500

    fazal

--- src/screens/Index.js
+++ src/screens/Index.js
@@ -1,4 +1,9 @@
 import BuyHes from './BuyHes';
 import TokenOverview from './TokenOverview';
 import Payments from './Payments';
-export {BuyHes,TokenOverview,Payments}
+import News from './News';
+import Disclaimer from './Disclaimer';
+import Partners from './Partners';
+import Team from './Team';
+import Admin from './Admin';
+export {BuyHes,TokenOverview,Payments,News,Disclaimer,Partners,Team,Admin}
--- src/screens/Payments.js
+++ src/screens/Payments.js
@@ -1,110 +1,187 @@
 import { Col, Container, Row } from "react-bootstrap";
-import Arrow from '../assets/images/arrow.png'
-import ArrowWhite from '../assets/images/arrow-white.png'
-import Hest from '../assets/images/hest.png'
-import Usdt from '../assets/images/usdt.png'
+import HorizontalLogo from '../assets/images/horizontal-logo.png'
+import {HistoricGraph} from '../components/Index';
 
 function Payments(){
     return <>
             <Container fluid>
                 <div className="page-margin-top">
                 <Row>
-                    <Col lg={6}>
-                       <div className="token-section">
-                           <div className="flex">
-                               <span>total supply <span className="green">(hest)</span>:</span>
-                               <div style={{width:"100px"}} className="custom-bar">
-                               .
-
-                               </div>
-                           </div>
-                           <div className="flex">
-                               <span>TOTAL LOCKED  <span className="green">(hest)</span>:</span>
-                               <div style={{width:"200px"}} className="custom-bar">
-                               .
-
-                               </div>
-                           </div>
-                           <div className="flex">
-                               <span>TOTAL DeFi Available  <span className="green">(hest)</span>:</span>
-                               <div style={{width:"200px"}} className="custom-bar">
-                               .
-
-                               </div>
-                           </div>
-                           <div className="flex">
-                               <span>TOTAL Centr. Fi Available  <span className="green">(hest)</span>:</span>
-                               <div style={{width:"200px"}} className="custom-bar">
-                               .
-
-                               </div>
-                           </div>
-                           <div className="flex">
-                               <span>TOTAL STAKED  <span className="green">(hest)</span>:</span>
-                               <div style={{width:"200px"}} className="custom-bar">
-                               .
-
-                               </div>
-                           </div>
-                           <div className="flex">
-                               <span>TOTAL SALED  <span className="green">(hest)</span>:</span>
-                               <div style={{width:"200px"}} className="custom-bar">
-                                   .
-                               </div>
-                           </div>
-                       </div>
+                    <Col lg={4}>
+                      <img src={HorizontalLogo}/>
+                      <p className="mt-1">in we believe in the cryptocurrencies for this reason, we created our own token, where we are sharing monthly a percentage of our earnings by our services and solutions offering into the energy sector, these payments come from our billing account, and it represents a way to support people that trust in our projects, activities and operations making them part of our growing up process.</p>
+                      <div className="mt-5">
+                      <div className="payment-first-flex">
+                        <div>
+                            <span>DATE OF LAST DEPOSIT:  <span className="green">12/02/2022</span></span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>TOTAL LAST DEPOSIT <span className="green">(USDT)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>DEPOSIT MADE TO SUPPORT HEST <span className="green">(USD)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>DEPOSIT MADE TO TEAM <span className="green">(USD)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>DEPOSIT MADE TO PARTNERSHIP <span className="green">(USD)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>TOTAL SHARING AMOUNT TO STAKE <span className="green">(USD)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>TOTAL SHARING AMOUNT TO STAKE <span className="green">(HEST)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                    <div className="payment-first-flex">
+                        <div>
+                            <span>TOTAL SALED <span className="green">(HEST)</span>:</span>
+                        </div>
+                        <div className="background-green">
+                            .
+                        </div>
+                    </div>
+                      </div>
                     </Col>
-                    <Col lg={6}>
+                    <Col lg={8}>
                        <div className="token-section">
                            <div className="balance-section">
-                               <h5 className="green">YOUR BALANCE</h5>
-                               <span>Balance of 0x0f7809</span>
+                               <h5 className="section-title">NEXT PAYMENT</h5>
+                               <span>WILL RELEASE IN :</span>
                            </div>
-                           <div className="balance-flex">
-                               <div className="inner">
-                               <span>TOTAL SUPPLY (HEST):</span>
+                           <div className="payment-flex">
+                               <div className="inner flex">
+                               <div className="timer-icon-section">
+                               <i class="fa-solid fa-stopwatch"></i>
                                </div>
-                               <div className="inner">
-                                   <div className="green-bar">
-                                       <span>1</span>
+                               <div className="inner flex-column">
+                                   <div>
+                                   <span>DAYS</span>
+                                   <span>HRS</span>
+                                   <span>MIN</span>
+                                   <span>SEC</span>
+                                   </div>
+                                   <div className="green-background timer-section">
+                                       <span>00</span>
+                                       <span>00</span>
+                                       <span>00</span>
+                                       <span>00</span>
+                                   </div>
+                               </div>
+                               </div>
+                              
+                               <div className="inner flex">
+                                   <div className="inner-flex">
+                                   <span>HEST WILL RECEIVE:</span>
+                                   </div>
+                                   <div className="flex-with-name">
+                                   <div className="inner-flex green-background amount-section">
+                                   <span>76,500.00</span>
+                                   </div>
+                                   <span className="name green">HEST</span>
                                    </div>
                                </div>
                            </div>
-                           <div className="balance-flex">
-                               <div className="inner">
-                               <span>VALUE:</span>
-                               </div>
-                               <div className="inner d-flex">
-                                   <div className="green-bar">
-                                       <span>0.0085</span>
+                           <div className="payment-flex">
+                               <div className="inner flex">
+                                   <div className="inner-flex">
+                                   <span>VALUE:</span>
                                    </div>
-                                   <span className="token-name">USD</span>
-                               </div>
-                           </div>
-                           <div className="balance-flex">
-                               <div className="inner">
-                               <span>STAKE BALANCE:</span>
-                               </div>
-                               <div className="inner d-flex">
-                                   <div className="green-bar">
-                                       <span>150</span>
+                                   <div className="flex-with-name">
+                                   <div className="inner-flex green-background amount-section">
+                                   <span>150,000.00</span>
                                    </div>
-                                   <span className="token-name">HEST</span>
-                               </div>
-                           </div>
-                           <div className="balance-flex">
-                               <div className="inner">
-                               <span>CLAIMABLE BALANCE:</span>
-                               </div>
-                               <div className="inner d-flex">
-                                   <div className="green-bar">
-                                       <span>23</span>
+                                   <span className="name green">USD</span>
                                    </div>
-                                   <span className="token-name">USD</span>
+                               </div>
+                               <div className="inner flex">
+                                   <div className="inner-flex">
+                                   <span>CLAIMABLE BALANCE:</span>
+                                   </div>
+                                   <div className="flex-with-name">
+                                   <div className="inner-flex green-background amount-section">
+                                   <span>58,500.00</span>
+                                   </div>
+                                   <span className="name green">USD</span>
+                                   </div>
                                </div>
                            </div>
                        </div>
+                       <div className="token-section mt-5">
+                           <div className="historic-flex">
+                           <div>
+                           <h5 className="title-section">Historic Graph</h5>
+                           <span>DEPOSITS</span>
+                           </div>
+                           <div className="filter">
+                               <div className="flex">
+                               <div>
+                                   <input type="checkbox"/>
+                                   <span>Team</span>
+                               </div>
+                               <div>
+                                   <input type="checkbox"/>
+                                   <span>All</span>
+                               </div>
+                               </div>
+                              <div className="flex">
+                              <div >
+                                   <input type="checkbox"/>
+                                   <span>Partners</span>
+                               </div>
+                               <div>
+                                   <input type="checkbox"/>
+                                   <span>Token</span>
+                               </div>
+                              </div>
+                              <div className="flex">
+                               <div>
+                                   <input type="checkbox"/>
+                                   <span>Stake - MiniPools</span>
+                               </div>
+                              </div>
+                           </div>
+                           </div>
+                           <div className="chart-section w-100">
+                               <HistoricGraph/>
+                           </div>
+                       </div>
                     </Col>
+                  
                 </Row>
                 </div>
             </Container>
